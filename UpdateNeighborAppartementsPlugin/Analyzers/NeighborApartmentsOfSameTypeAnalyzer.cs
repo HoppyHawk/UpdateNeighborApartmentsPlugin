3a83cd99fa4e4ff9df77667720f2bfe6540a6ad1
hoppyhawk <vityastrebov@gmail.com>
2023-07-01 16:50:27 +0300

    Refactor NeighborApartmentsAnalyzer to use NodeCombinationsCalculator

--- UpdateNeighborAppartementsPlugin/Analyzers/NeighborApartmentsOfSameTypeAnalyzer.cs
+++ UpdateNeighborAppartementsPlugin/Analyzers/NeighborApartmentsOfSameTypeAnalyzer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using UpdateNeighborAppartementsPlugin.Analyzers.Calculators;
 using UpdateNeighborAppartementsPlugin.DocumentTreeModel.Nodes;
 using UpdateNeighborAppartementsPlugin.Model;
 
@@ -11,48 +12,16 @@ namespace UpdateNeighborAppartementsPlugin.Analyzers
         public List<DocumentTreeNode> Analyze(IEnumerable<DocumentTreeNode> nodes, 
             INodeCombinationsFilter combinationsFilter)
         {
-            var allNeighboringCombinations = CollectAllNeighboringCombinations(nodes);
+            var nodeComparer = new NeighborApartmentsComparer();
+            var combinationsCalculator = new NodeCombinationsCalculator(nodeComparer);
 
-            var matchingNodes = new List<DocumentTreeNode>();
-
-            foreach (var neighboringCombinations in allNeighboringCombinations)
-            {
-                var filteredResults = combinationsFilter.Apply(neighboringCombinations);
-
-                foreach (var node in filteredResults)
-                {
-                    matchingNodes.Add(node);
-                }
-            }
-
-            return matchingNodes;
-        }
-
-        private IEnumerable<IEnumerable<IEnumerable<ApartmentNode>>> CollectAllNeighboringCombinations(
-            IEnumerable<DocumentTreeNode> nodes)
-        {
             var apartmentNodesByApartmentType = CollectChildNodes(ParameterKeys.ROM_SubZone, nodes);
-            foreach (var group in apartmentNodesByApartmentType)
-            {
-                var apartmentGroup = group.Select(n => n as ApartmentNode);
-                yield return FindNeighboringCombinations(apartmentGroup);
-            }
-        }
 
-        private IEnumerable<IEnumerable<ApartmentNode>> FindNeighboringCombinations(
-            IEnumerable<ApartmentNode> apartments)
-        {
-            var neighborApartments = apartments.Select(
-                   a1 => apartments.Where(a2 => IsNeighboringCombination(a1, a2))
-                       .OrderBy(a => a.ApartmentNumber)
-               )
-               .Where(c => c.Count() > 1);
-            return neighborApartments;
-        }
+            var allNeighboringCombinations = apartmentNodesByApartmentType
+                .Select(g => combinationsCalculator.Calculate(g));
 
-        private bool IsNeighboringCombination(ApartmentNode apartment1, ApartmentNode apartment2)
-        {
-            return Math.Abs(apartment1.ApartmentNumber - apartment2.ApartmentNumber) <= 1;
+            return allNeighboringCombinations.SelectMany(c => combinationsFilter.Apply(c)).ToList();
+
         }
     }
 }
