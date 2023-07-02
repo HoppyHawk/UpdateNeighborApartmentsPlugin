948cdb1c5db151db636c1e9df8e10f9358d2211c
hoppyhawk <vityastrebov@gmail.com>
2023-07-02 20:06:40 +0300

    Convert found neighbors to list

--- UpdateNeighborAppartementsPlugin/Analyzers/Filters/CompositeNodeCombinationsFilter.cs
+++ UpdateNeighborAppartementsPlugin/Analyzers/Filters/CompositeNodeCombinationsFilter.cs
@@ -19,7 +19,7 @@ namespace UpdateNeighborAppartementsPlugin.Analyzers
         public IEnumerable<DocumentTreeNode> Apply(IEnumerable<IEnumerable<DocumentTreeNode>> nodeCombinations)
         {
             var distinctNodes = distinctNodeFilter.Apply(nodeCombinations).ToList();
-            var firstNeighbors = firstNeighborFilter.Apply(nodeCombinations);
+            var firstNeighbors = firstNeighborFilter.Apply(nodeCombinations).ToList();
             distinctNodes.ForEach(n => n.RequiresProcessing = firstNeighbors.Contains(n));
             return distinctNodes;
         }
